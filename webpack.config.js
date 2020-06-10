fa8e0dc9097d1b8e550df03ad7a8cc5da15083da
大卫 <1499782633@qq.com>
2020-06-10 09:20:30 +0800

    安装url-loader、file-loader模块，配置webpack，使其能够打包图片

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     entry: "./webpack1/src/main.js",
     output: {
         path: path.resolve(__dirname, "webpack1/dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        // 当图片的大小超过limit设定的值时，会用到file-loader模块，它会将图片直接打成包
+        publicPath: "dist/"
     },
     module: {
         rules: [
@@ -21,6 +23,18 @@ module.exports = {
                 }, {
                     loader: "less-loader" // compiles Less to CSS
                 }]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // 图片大小小于limit值时，图片被打包成base64字符串
+                            limit: 23000
+                        }
+                    }
+                ]
             }
         ]
     }
